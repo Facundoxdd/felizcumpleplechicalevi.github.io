666994f81a564e41a1ad3c31c8cdf43b2cc399a0
Facundoxdd <matias14gonzalezpaz@gmail.com>
2024-10-21 02:34:11 -0300

    Add files via upload

---
+++ boton.js
@@ -0,0 +1,8 @@
+function xd(){
+    document.getElementById("mine")[0];
+    if(container.style.display == "none"){
+        container.style.display = "flex";
+    }else{
+        container.style.display = "none";
+    }
+}
